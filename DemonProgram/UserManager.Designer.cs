2934c54c813d823c1f8c561cc701cab2fa862106
YeongJu-Na <skdudwn7@gmail.com>
2021-05-07 14:42:39 +0900

    daemon

---
+++ DemonProgram/UserManager.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace DemonProgram
+{
+    partial class UserManager
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inList = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.outList = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(1, 1);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.inList);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.outList);
+            this.splitContainer1.Size = new System.Drawing.Size(424, 444);
+            this.splitContainer1.SplitterDistance = 213;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Users";
+            // 
+            // inList
+            // 
+            this.inList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inList.FormattingEnabled = true;
+            this.inList.ItemHeight = 12;
+            this.inList.Location = new System.Drawing.Point(3, 27);
+            this.inList.Name = "inList";
+            this.inList.Size = new System.Drawing.Size(418, 184);
+            this.inList.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Disconnect Users";
+            // 
+            // outList
+            // 
+            this.outList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outList.FormattingEnabled = true;
+            this.outList.ItemHeight = 12;
+            this.outList.Location = new System.Drawing.Point(3, 27);
+            this.outList.Name = "outList";
+            this.outList.Size = new System.Drawing.Size(418, 196);
+            this.outList.TabIndex = 1;
+            // 
+            // UserManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 446);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "UserManager";
+            this.Text = "User Manager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UserManager_FormClosing);
+            this.Load += new System.EventHandler(this.UserManager_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox inList;
+        private System.Windows.Forms.ListBox outList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DemonProgram/UserManager.cs
@@ -0,0 +1,124 @@
+﻿using MyLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemonProgram
+{
+    public partial class UserManager : Form
+    {
+        public UserManager()
+        {
+            InitializeComponent();
+        }
+        byte[] ServerIP = { 192, 168, 0, 52 };
+        string ServerPort = "9000";
+
+        List<Socket> socks = new List<Socket>();
+        Socket sock = null;
+        Socket sockServer = null;
+
+        Thread threadServer = null;
+        Thread threadSession = null;
+
+        delegate void cbAddList(string str, bool isIn);
+
+        void AddList(string str, bool isIn)
+        {
+            if (inList.InvokeRequired || outList.InvokeRequired)
+            {
+                cbAddList cb = new cbAddList(AddList);
+                Invoke(cb, new object[] { str, isIn });
+            }
+            else
+            {
+                if (isIn) inList.Items.Add(str);
+                else
+                {
+                    outList.Items.Add(str);
+                    inList.Items.Remove(str);
+                }
+            }
+        }
+
+        private void UserManager_Load(object sender, EventArgs e)
+        {
+            sockServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            threadServer = new Thread(ServerProcess);
+            threadServer.IsBackground = true;    //주 스레드 종료시 함께 종료
+            threadSession = new Thread(SessionProcess);
+            threadSession.IsBackground = true;
+
+            threadServer.Start();
+            threadSession.Start();
+        }
+
+        void ServerProcess()
+        {
+            IPAddress ip = new IPAddress(ServerIP);
+            IPEndPoint ep = new IPEndPoint(ip, int.Parse(ServerPort));
+            sockServer.Bind(ep);
+            sockServer.Listen(50000);
+            try
+            {
+                while (true)
+                {
+                    sock = sockServer.Accept();
+
+                    if (sock != null)
+                    {
+                        string[] sArr = sock.RemoteEndPoint.ToString().Split(':');
+                        AddList(sock.RemoteEndPoint.ToString(), true);
+                        socks.Add(sock);
+                    }
+                    Thread.Sleep(100);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        void SessionProcess()
+        {
+            while (true)
+            {
+                List<Socket> removelist = new List<Socket>();
+
+                foreach (Socket ss in socks)
+                {
+                    if (!mylib.isAlive(ss))
+                    {
+                        removelist.Add(ss);
+                    }
+                }
+                foreach (Socket ss in removelist)
+                {
+                    AddList(ss.RemoteEndPoint.ToString(), false);
+                    socks.Remove(ss);
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        private void UserManager_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (sockServer != null) sockServer.Close();
+            if (sock != null) sock.Close();
+            foreach (Socket ss in socks) ss.Close();
+
+            if (threadServer != null) threadServer.Abort();
+            if (threadSession != null) threadSession.Abort();
+        }
+    }
+}
